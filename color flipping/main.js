b3c6e2ae96e69eab1aa189242a031308731e68c4
anwesabiswas <anwesabiswasce@gmail.com>
2022-04-21 11:54:36 +0530

    colour

--- color flipping/main.js
+++ color flipping/main.js
@@ -1,18 +1,30 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn_random");
 const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        // let index = getIndex();
-        hexColor = hexColor + hex[Math.floor(Math.random() * hex.length)];
+
+        hexColor = hexColor + hex[anyNumber()];
     };
-    // console.log(hexColor);
+
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 
 })
 
-// function getIndex() {
-//     return Math.floor(Math.random() * hex.length);
-// }
+function changeColor(color) {
+    if (color === 'Red') {
+        document.body.style.backgroundColor = 'red';
+    } else if (color === 'Blue') {
+        document.body.style.backgroundColor = 'blue';
+    } else if (color === 'Yellow') {
+        document.body.style.backgroundColor = 'yellow';
+    } else {
+        document.body.style.backgroundColor = 'green';
+    }
+}
+
+function anyNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
